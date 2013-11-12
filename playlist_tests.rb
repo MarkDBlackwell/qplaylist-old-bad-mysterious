080b57449ce97383ce755fdf2a4a2ae70987f792
Mark D. Blackwell <markdblackwell01@gmail.com>
2013-11-12 08:48:46 -0500

    Organize for tests

---
+++ playlist_tests.rb
@@ -0,0 +1,22 @@
+require './playlist_classes'
+require 'minitest/autorun'
+
+module Playlist
+  class TestSnapshot
+  end
+
+  class TestSubstitutions
+  end
+
+  class TestNowPlayingSubstitutions
+  end
+
+  class TestLatestFiveSubstitutions
+  end
+
+  class TestSongs
+  end
+
+  class TestRun
+  end
+end #module
